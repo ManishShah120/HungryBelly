cbe05a4a2d4c2dd8aa6beb3c55e011d023f5a3f4
ManishShah120 <mkshah141@gmail.com>
2021-03-21 22:00:28 +0530

    Will resume the work Once I am Free.

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'food_type', 'location', 'quanity', 'lifetime', 'bookingstatus')
+
+    def __str__(self):
+        return self.food_type
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    food_type = models.CharField(max_length=100)
+    location = models.CharField(max_length=500)
+    quanity = models.IntegerField()
+    lifetime = models.DateField()
+    bookingstatus = models.BooleanField()
